ac37ba1f280e252547c108a32483d37cbb4f3f4d
HugoG <hugo.guerquin@outlook.fr>
2021-03-03 11:12:58 +0100

    HF : ajout d'un bool pour load les scènes supp
    pour pouvoir play sans à virer toutes les scènes

--- Assets/Game/Scripts/WSB_SceneLoader.cs
+++ Assets/Game/Scripts/WSB_SceneLoader.cs
@@ -7,10 +7,13 @@ using UnityEngine.SceneManagement;
 public class WSB_SceneLoader : MonoBehaviour
 {
     [SerializeField] List<string> allScenesInOrder = new List<string>();
-
+    [SerializeField] bool doLoad = true;
 
     void Start()
     {
+        if (!doLoad)
+            return;
+
         for (int i = 0; i < allScenesInOrder.Count; i++)
         {
             SceneManager.LoadScene(allScenesInOrder[i], LoadSceneMode.Additive);
