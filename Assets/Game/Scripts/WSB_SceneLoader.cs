7eeee5946deab6a07396206d520b591752d101f0
DESKTOP-PJTDPH8\Loulou <louise.jeanson.heriard@gmail.com>
2021-03-03 15:23:57 +0100

    Revert "HF : ajout d'un bool pour load les scènes supp"
    This reverts commit ac37ba1f280e252547c108a32483d37cbb4f3f4d.

--- Assets/Game/Scripts/WSB_SceneLoader.cs
+++ Assets/Game/Scripts/WSB_SceneLoader.cs
@@ -7,13 +7,10 @@ using UnityEngine.SceneManagement;
 public class WSB_SceneLoader : MonoBehaviour
 {
     [SerializeField] List<string> allScenesInOrder = new List<string>();
-    [SerializeField] bool doLoad = true;
+
 
     void Start()
     {
-        if (!doLoad)
-            return;
-
         for (int i = 0; i < allScenesInOrder.Count; i++)
         {
             SceneManager.LoadScene(allScenesInOrder[i], LoadSceneMode.Additive);
