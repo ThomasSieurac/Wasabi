1506799a5575a6c3ff14c86fa8a25fbe4adc939b
ThomasSieurac <sreflex.2001@gmail.com>
2021-04-06 09:22:38 +0200

    MUSIC
    Lvl 1

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,15 +11,46 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUS_LEVEL1 = 1800177509U;
+        static const AkUniqueID STOP_MUS_LEVEL1 = 4037185023U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID WASABI_MUS_SB = 1235495070U;
         static const AkUniqueID WASABI_WU_NS = 3291955309U;
     } // namespace BANKS
 
     namespace BUSSES
     {
+        static const AkUniqueID BAN = 513390140U;
+        static const AkUniqueID BAN_FOLEY = 487774080U;
+        static const AkUniqueID BAN_FT = 44542721U;
+        static const AkUniqueID ENV = 529726550U;
+        static const AkUniqueID FDB = 1267691439U;
+        static const AkUniqueID GH1 = 428663473U;
+        static const AkUniqueID GH2 = 428663474U;
+        static const AkUniqueID GH3 = 428663475U;
+        static const AkUniqueID HUB = 646625282U;
+        static const AkUniqueID INCOMMON = 3070449207U;
+        static const AkUniqueID INTERRACTION = 3217650583U;
+        static const AkUniqueID LUX = 914478924U;
+        static const AkUniqueID LUX_FOLEY = 4096139792U;
+        static const AkUniqueID LUX_FT = 2830372017U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID NPC = 662417162U;
+        static const AkUniqueID OTHER = 2376466361U;
+        static const AkUniqueID RUNES = 500672212U;
+        static const AkUniqueID SEEDS = 3750040961U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID VOC = 543694031U;
+        static const AkUniqueID VOC_BAN = 3806204025U;
+        static const AkUniqueID VOC_LUX = 4075528361U;
+        static const AkUniqueID VOC_OTHER = 2759895900U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
